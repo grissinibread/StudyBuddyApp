b6407271ebdf6d1813e4c4e92d171f26587ab73e
Alejandro Navarro <94933917+grissinibread@users.noreply.github.com>
2024-11-08 15:12:39 -0800

    Add files via upload

---
+++ Code/view/DiscoverPage.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DiscoverPage extends JPanel {
+    public DiscoverPage() {
+        setLayout(new GridLayout());
+    }
+}
