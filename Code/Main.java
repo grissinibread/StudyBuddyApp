f695ada7a852b73ad35c3575d5e0371ddedde97c
Alejandro Navarro <alexnavarro57@gmail.com>
2024-11-15 16:37:10 -0800

    Minor aesthetic updates!

--- Code/Main.java
+++ Code/Main.java
@@ -6,4 +6,5 @@ public class Main {
     public static void main(String[] args) throws IOException {
         AppWindow appWindow = new AppWindow();
         appWindow.setVisible(true); //Displays the window
+
 }
