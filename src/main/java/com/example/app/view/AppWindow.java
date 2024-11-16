f4deb1685c1fc42da8c77d6b86f3f9136cad72f4
SrAxeMan <110641220+SrAxeMan@users.noreply.github.com>
2024-11-16 13:48:33 -0800

    Update AppWindow.java
    Adds Buttons through AppWindow to cycle through one instance of each page.

--- src/main/java/com/example/app/view/AppWindow.java
+++ src/main/java/com/example/app/view/AppWindow.java
@@ -2,6 +2,8 @@ package main.java.com.example.app.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
 import java.io.File;
 import java.io.IOException;
@@ -10,6 +12,7 @@ public class AppWindow extends JFrame {
     public AppWindow() {
         setTitle("Study Buddy");
         setUndecorated(true); // removes default title bar
+        GridBagConstraints frameConstraints = new GridBagConstraints();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
@@ -37,5 +40,42 @@ public class AppWindow extends JFrame {
 
         MessagesPage messagesPage = new MessagesPage();
         //add(messagesPage);
+
+        //Sign Up Button
+        JButton signUpButton = new JButton("Sign Up");
+        signUpButton.setBackground(Color.decode("#F8E71C"));
+        frameConstraints.gridx = 0;
+        frameConstraints.gridy = 8;
+        loginPage.add(signUpButton, frameConstraints);
+
+        // When Sign Up Button is pressed, it takes you to the Sign Up Page.
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openPage(signUpPage);
+            }
+        });
+
+        //Makes and displays a Back to Login Button.
+        JButton backToLoginButton = new JButton("Back to Login");
+        frameConstraints.gridx = 3;
+        frameConstraints.gridy = 8;
+        signUpPage.add(backToLoginButton, frameConstraints);
+
+        // Pressing the "Back to Login" button takes the user
+        // back to the login screen.
+        backToLoginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openPage(loginPage);
+            }
+        });
+    }
+
+    public void openPage(JPanel page)
+    {
+        setContentPane(page);
+        revalidate();
+        repaint();
     }
 }
