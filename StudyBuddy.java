ad379ed8445d7825d68f1633827275962451d0a5
SrAxeMan <110641220+SrAxeMan@users.noreply.github.com>
2024-11-17 21:23:13 -0800

    Add files via upload

---
+++ StudyBuddy.java
@@ -0,0 +1,10 @@
+package com.example.app;
+
+import com.example.app.view.AppWindow;
+
+public class StudyBuddy {
+    public static void main(String[] args) {
+        AppWindow appWindow = AppWindow.getAppWindow();
+        appWindow.setVisible(true); //Displays the window
+    }
+}
