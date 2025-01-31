6c8f375565e2faaa9677505276cf6348fc3ed525
Alejandro Navarro <alejandronavarro@Mac.attlocal.net>
2025-01-31 00:21:30 -0800

    Initial commit (deleted all contents)

--- src/main/java/com/example/app/StudyBuddy.java
+++
@@ -1,9 +0,0 @@
-package com.example.app;
-import com.example.app.view.AppWindow;
-
-public class StudyBuddy {
-    public static void main(String[] args) {
-        // Initializes the GUI
-        AppWindow.launch(AppWindow.class, args);
-    }
-}
