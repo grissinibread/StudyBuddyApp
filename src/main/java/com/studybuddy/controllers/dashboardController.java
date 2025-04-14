024e0c2e269d5227e706d71b9e9aa17cdc4483d2
updawg420 <christine05789@gmail.com>
2025-04-13 20:35:51 -0700

    Log Out Button Action
    Pair Programming with special guest Ale Bread on Dashboard Controller File

--- src/main/java/com/studybuddy/controllers/dashboardController.java
+++ src/main/java/com/studybuddy/controllers/dashboardController.java
@@ -1,14 +1,26 @@
 package com.studybuddy.controllers;
 
+import com.studybuddy.models.Model;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class dashboardController implements Initializable {
+
+    @FXML
+    public Button dashboardLogOutButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Dashboard controller initialized!");
+
+        // Revert to Login page
+        dashboardLogOutButton.setOnAction(event -> {});
+        System.out.println("Dashboard Log Out Button Clicked!");
     }
 }
