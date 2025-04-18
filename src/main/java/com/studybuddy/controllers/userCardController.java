0b1a6edcd68ab3e9eebfecb5871f102bca9abcad
Alejandro Navarro <alejandronavarro@Mac.attlocal.net>
2025-04-18 00:46:25 -0700

    Updated dashboard to include userCards

---
+++ src/main/java/com/studybuddy/controllers/userCardController.java
@@ -0,0 +1,28 @@
+package com.studybuddy.controllers;
+
+import javafx.fxml.FXML;
+
+public class userCardController {
+    @FXML
+    private String userCard_Name;
+
+    @FXML
+    private String userCard_Major;
+
+    @FXML
+    private String userCard_GraduationYear;
+
+    //TODO: finish up these functions
+    public void setUserCardName() {
+
+    }
+
+    public void setUserCardMajor() {
+
+    }
+
+    public void setUserCardGraduationYear() {
+
+    }
+
+}
