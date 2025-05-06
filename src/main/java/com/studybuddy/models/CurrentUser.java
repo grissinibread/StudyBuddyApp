1b36a185d87a198a2ac91a3c36b24f6bb34faa23
Alejandro Navarro <alexnavarro57@gmail.com>
2025-05-05 19:53:39 -0700

    Added local user data

---
+++ src/main/java/com/studybuddy/models/CurrentUser.java
@@ -0,0 +1,96 @@
+package com.studybuddy.models;
+
+import java.util.List;
+
+public class CurrentUser {
+    private String id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String major;
+    private Integer gradYear;
+    private List<String> interests;
+    private String bio;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public Integer getGradYear() {
+        return gradYear;
+    }
+
+    public void setGradYear(Integer gradYear) {
+        this.gradYear = gradYear;
+    }
+
+    public List<String> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<String> interests) {
+        this.interests = interests;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
