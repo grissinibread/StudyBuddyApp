56bdae7af6bd4a63b26f78f84468927f15d386f7
Alejandro Navarro <alexnavarro57@gmail.com>
2025-05-05 19:32:15 -0700

    Small visual updates to connections

--- src/main/java/com/studybuddy/controllers/connectionsController.java
+++ src/main/java/com/studybuddy/controllers/connectionsController.java
@@ -1,13 +1,21 @@
 package com.studybuddy.controllers;
 
+import com.studybuddy.models.Model;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class connectionsController implements Initializable {
+    @FXML
+    private Button connections_dashboardButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("connectionsController initialized!");
+        connections_dashboardButton.setOnAction(event -> {
+            Model.getInstance().getViewFactory().showDashboard();
+        });
     }
 }
