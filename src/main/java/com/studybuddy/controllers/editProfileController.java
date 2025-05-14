6f496881fc47f3ab1cad8e0329864671d9977734
updawg420 <christine05789@gmail.com>
2025-05-14 11:55:04 -0700

    Working buttons(cancel & save) for edit profile

--- src/main/java/com/studybuddy/controllers/editProfileController.java
+++ src/main/java/com/studybuddy/controllers/editProfileController.java
@@ -1,4 +1,31 @@
 package com.studybuddy.controllers;
 
+import javafx.scene.control.Button;
+
+import com.studybuddy.models.Model;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
 public class editProfileController {
+    @FXML
+    public Button editProf_saveButton;
+    public Button editProf_cancelButton;
+
+    public void initialize() {
+        editProf_saveButton.setOnAction(actionEvent -> {
+            System.out.println("Save button pressed!");
+            // TODO: Implement save edited profile functionality
+        });
+        editProf_cancelButton.setOnAction(actionEvent -> {
+            System.out.println("Cancel button pressed!");
+            Model.getInstance().getViewFactory().showProfile();
+        });
+
+//    editProf_saveButton.setOnAction(actionEvent -> {
+//        System.out.println("Save button clicked!");
+
+
+    }
+
 }
