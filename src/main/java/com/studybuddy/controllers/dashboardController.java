747fe6c73d6e57916b0022669a7634f3fa18abe1
adria <varga418@csusm.edu>
2025-05-13 20:22:19 -0700

    matches passed and can be scrolled through

--- src/main/java/com/studybuddy/controllers/dashboardController.java
+++ src/main/java/com/studybuddy/controllers/dashboardController.java
@@ -1,23 +1,24 @@
 package com.studybuddy.controllers;
 
 import com.studybuddy.models.Model;
+import com.studybuddy.models.Match;
+import com.studybuddy.services.MatchIterator;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.control.Label;  // Ensure you're importing Label
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Random;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import static com.studybuddy.services.ApiClient.retreiveAllMatches;
-import static com.studybuddy.services.ApiClient.retreiveAllUsers;
 
 public class dashboardController implements Initializable {
-
+    private int startingIndex = 0; //index to track starting point for outputted matches
+    private MatchIterator iterator;
     @FXML
     public Button dashboard_LogoutButton;
 
@@ -40,8 +41,9 @@ public class dashboardController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Dashboard controller initialized!");
-
-        populateUserCards();
+        iterator = new MatchIterator(retreiveAllMatches());
+        populateUserCards(iterator.getBatch(4));
+        //startingIndex += 4;
 
         // Revert to Login page
         dashboard_LogoutButton.setOnAction(event -> {
@@ -60,38 +62,35 @@ public class dashboardController implements Initializable {
         });
 
         refreshMatches.setOnAction(actionEvent -> {
-            populateUserCards();
+            populateUserCards(iterator.getBatch(4));
         });
     }
 
-    private void populateUserCards() {
-        var currentUser = Model.getInstance().getCurrentUser();
-        System.out.println("Current user in discover: " + currentUser.getFirstName() + " " + currentUser.getEmail());
-        var users = retreiveAllMatches();
-        for (var user : users) {
-            //System.out.println(user.getFirstName() + " " + user.getLastName());
-            System.out.println(user.getFirstName());
-        }
+    private void populateUserCards(List<Match> users) {
         AnchorPane[] userCards = {user1, user2, user3, user4};
 
-        for (int i = 0; i < users.size() && i < userCards.length; i++) {
+        for (int i = 0; i < userCards.length; i++) {
             try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/userCard.fxml"));
                 AnchorPane userCard = loader.load();
                 userCardController controller = loader.getController();
 
-                Random random = new Random();
-                int randomIndex = random.nextInt(users.size());
+                var match = users.get(i);
+                if (match.getFirstName() == null)
+                    controller.userName.setText("");
+                else
+                    controller.userName.setText(match.getFirstName());
+                if (match.getMajor() == null)
+                    controller.major.setText("");
+                else
+                    controller.major.setText(match.getMajor());
+                if (match.getGradYear() == null)
+                    controller.gradYear.setText("");
+                else
+                    controller.gradYear.setText(match.getGradYear().toString());
 
-                // Set user information using Labels
-                controller.userName.setText(users.get(randomIndex).getFirstName());
-                //controller.userName.setText(users.get(randomIndex).getFirstName() + " " + users.get(randomIndex).getLastName());
-                controller.major.setText(users.get(randomIndex).getMajor());
-                controller.gradYear.setText(users.get(randomIndex).getGradYear().toString());
-
-                // Set interests using Labels
-                if (users.get(randomIndex).getInterests() != null && !users.get(randomIndex).getInterests().isEmpty()) {
-                    String[] interests = users.get(randomIndex).getInterests().toArray(new String[0]);
+                if (match.getInterests() != null && !match.getInterests().isEmpty()) {
+                    String[] interests = match.getInterests().toArray(new String[0]);
                     if (interests.length > 0) controller.interest1.setText(interests[0]);
                     if (interests.length > 1) controller.interest2.setText(interests[1]);
                     if (interests.length > 2) controller.interest3.setText(interests[2]);
@@ -101,7 +100,6 @@ public class dashboardController implements Initializable {
                     controller.interest3.setText("");
                 }
 
-                // Add the user card to the corresponding AnchorPane
                 userCards[i].getChildren().setAll(userCard);
             } catch (IOException e) {
                 e.printStackTrace();
---
+++ src/main/java/com/studybuddy/services/MatchIterator.java
@@ -0,0 +1,34 @@
+package com.studybuddy.services;
+
+import com.studybuddy.models.Match;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchIterator {
+    private final List<Match> matches = new ArrayList<>();
+    private int index = 0;
+
+    public MatchIterator(List<Match> matches) {
+        this.matches.addAll(matches);
+    }
+
+    public List<Match> getBatch(int batchSize) {
+        List<Match> batch = new ArrayList<>();
+        for (int i = 0; i < batchSize; i++) {
+            if (index >= matches.size()) {
+                // no more matches
+                batch.add(null);
+            } else {
+                batch.add(matches.get(index));
+                index++;
+            }
+        }
+        // start again
+        if (index >= matches.size()) {
+            index = 0;
+        }
+        return batch;
+    }
+
+}
